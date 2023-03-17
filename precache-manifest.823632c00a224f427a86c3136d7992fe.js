02ecda0c443738b6baef6d40b847482ad51a34d4
Sai Teja Nadakuditi <saitejanadakuditi22@gmail.com>
2023-03-16 23:01:16 -0500

    Updates

---
+++ precache-manifest.823632c00a224f427a86c3136d7992fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b66f456730c41fe2086d",
+    "url": "/SaiTejaNadakuditiPortfolio/static/css/main.35d3924e.chunk.css"
+  },
+  {
+    "revision": "b66f456730c41fe2086d",
+    "url": "/SaiTejaNadakuditiPortfolio/static/js/main.b66f4567.chunk.js"
+  },
+  {
+    "revision": "bf3ed5239e269d7b95f8",
+    "url": "/SaiTejaNadakuditiPortfolio/static/js/1.bf3ed523.chunk.js"
+  },
+  {
+    "revision": "8fa2f7ccd0097cad3c45",
+    "url": "/SaiTejaNadakuditiPortfolio/static/js/runtime~main.8fa2f7cc.js"
+  },
+  {
+    "revision": "c18e40e37cf0ac32a3ae1955461f9eb4",
+    "url": "/SaiTejaNadakuditiPortfolio/index.html"
+  }
+];
